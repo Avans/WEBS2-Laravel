72b65f81a4313fed8d0c134030ec3c11585d963b
Rik Meijer <ha.meijer@avans.nl>
2019-01-29 13:38:56 +0100

    remove duplicate constant

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 class CalendarController extends Controller
 {
+    const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
     public function showYear($yearIndex = "Y")
     {
         return view('calendar.year', [
-            "month" => new \App\Calendar\Month(new \DateTime(date("$yearIndex-1-1")), ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
+            "month" => new \App\Calendar\Month(new \DateTime(date("$yearIndex-1-1")), self::DAYS)
         ]);
     }
 
     public function showMonth($monthIndex)
     {
         return view('calendar.month', [
-            "month" => new \App\Calendar\Month(new \DateTime(date($monthIndex."-1")), ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"])
+            "month" => new \App\Calendar\Month(new \DateTime(date($monthIndex."-1")), self::DAYS)
         ]);
     }
 }
